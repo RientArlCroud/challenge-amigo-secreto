630a8cb59c16426fe0575fe72c764115f7afef44
Juan Rodríguez <juanesx97@gmail.com>
2025-08-25 23:02:55 -0500

    Se agregó la función del botón 'añadir'

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigosIngresados = [];
+
+function agregarAmigo() {
+    let amigoAgregado = document.getElementById('amigo');
+    if (amigoAgregado.value == ""){
+        alert("Por favor, inserte un nombre");
+    }else {
+        amigosIngresados.push(amigoAgregado.value);
+        console.log(amigosIngresados);
+    }
+    amigoAgregado.value = "";
+    return;
+}
+
